f09bc2c1e3d86c114fbc93ba9d67051ef712bfda
davidsharp <hello@davidsharp.codes>
2022-09-29 16:06:41 +0100

    Rough start on resident data

---
+++ residents.lua
@@ -0,0 +1,32 @@
+-- just a dictionary of characters
+
+downstairsBloke = {
+  sprite = 'person',
+  success = 'Great work today, I look forward to seeing how you do tomorrow',
+  failure = 'Hard luck, come back refreshed tomorrow'
+  dialog = {
+    {
+      'Hey, so you must be the new builder',
+      'I have it here that you\'re contracted for a month?',
+      'Each day you\'ll need to build a new floor and a new home',
+      'But be careful! There are reports that our competition,',
+      'has been sneaking dangerous material into our supplies',
+      'and if they touch they could explode and set back your day\'s build'
+    }
+  }
+}
+
+residents = {
+  {
+    sprite = 'person',
+    request = 'Build me a home, plz',
+    success = 'Wow, thanks!',
+    failure = 'Dang, I\'ll have to find a home elsewhere',
+    dialog = {
+      {'todo'},
+      {'todo'},
+      {'todo'},
+      {'todo'},
+    }
+  }
+}
