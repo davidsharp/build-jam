3b6406e4ee6081f3eed340645bc7803128252287
davidsharp <hello@davidsharp.codes>
2022-10-02 13:37:41 +0100

    Draw blocks around all the logo letters

--- logo.lua
+++ logo.lua
@@ -14,22 +14,35 @@ function logo(x,y)
     o = getTile(k_tiles_trans,offset_x + 1,offset_y + 2),
     c = getTile(k_tiles_trans,offset_x + 2,offset_y + 1),
     k = getTile(k_tiles_trans,offset_x + 10,offset_y + 1),
-    s = getTile(k_tiles_trans,offset_x + 5,offset_y + 2)
+    s = getTile(k_tiles_trans,offset_x + 5,offset_y + 2),
+    block = getTile(k_tiles_trans,offset_x + 4,offset_y - 3)
   }
   
   love.graphics.draw(k_tiles_trans, quads.b, x, y)
+  love.graphics.draw(k_tiles_trans, quads.block, x, y)
   love.graphics.draw(k_tiles_trans, quads.u, x+(16*1), y)
+  love.graphics.draw(k_tiles_trans, quads.block, x+(16*1), y)
   love.graphics.draw(k_tiles_trans, quads.i, x+(16*2), y)
+  love.graphics.draw(k_tiles_trans, quads.block, x+(16*2), y)
   love.graphics.draw(k_tiles_trans, quads.l, x+(16*3), y)
+  love.graphics.draw(k_tiles_trans, quads.block, x+(16*3), y)
   love.graphics.draw(k_tiles_trans, quads.d, x+(16*4), y)
+  love.graphics.draw(k_tiles_trans, quads.block, x+(16*4), y)
 
   love.graphics.draw(k_tiles_trans, quads.n, x+(16*2), y+16)
+  love.graphics.draw(k_tiles_trans, quads.block, x+(16*2), y+16)
 
   love.graphics.draw(k_tiles_trans, quads.b, x-8, y+32)
+  love.graphics.draw(k_tiles_trans, quads.block, x-8, y+32)
   love.graphics.draw(k_tiles_trans, quads.l, x-8+(16*1), y+32)
+  love.graphics.draw(k_tiles_trans, quads.block, x-8+(16*1), y+32)
   love.graphics.draw(k_tiles_trans, quads.o, x-8+(16*2), y+32)
+  love.graphics.draw(k_tiles_trans, quads.block, x-8+(16*2), y+32)
   love.graphics.draw(k_tiles_trans, quads.c, x-8+(16*3), y+32)
+  love.graphics.draw(k_tiles_trans, quads.block, x-8+(16*3), y+32)
   love.graphics.draw(k_tiles_trans, quads.k, x-8+(16*4), y+32)
+  love.graphics.draw(k_tiles_trans, quads.block, x-8+(16*4), y+32)
   love.graphics.draw(k_tiles_trans, quads.s, x-8+(16*5), y+32)
+  love.graphics.draw(k_tiles_trans, quads.block, x-8+(16*5), y+32)
 
 end
