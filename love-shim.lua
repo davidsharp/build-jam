8184fa1c1531edcb5a7990c10e1e4b5b72034f45
David Sharp <hello@davidsharp.codes>
2022-11-02 14:27:45 +0000

    Scaffold out most of shim

--- love-shim.lua
+++ love-shim.lua
@@ -1,6 +1,14 @@
+-- import _before_ main
+
 love = {}
 
+local TOOD = function()
+  print('TODO')
+end
+
 love.graphics = {}
+
+love.graphics.newImage = TODO
 
 love.graphics.newQuad = function(x, y, w, h, tiles)
   -- fauxQuad, just returns dimensions
@@ -8,4 +16,39 @@ love.graphics.newQuad = function(x, y, w, h, tiles)
 end
 
 love.graphics.draw = function(tiles, fauxQuad, x, y)
+  TODO()
 end
+
+love.graphics.setColor = TODO
+
+love.graphics.rectangle = TODO
+
+love.graphics.print = TODO
+
+-- no-op, Used for Love2D only
+love.graphics.scale = function() end
+
+love.audio = {}
+
+love.audio.newSource = TODO
+
+love.keyboard = {}
+
+love.keyboard.isPressed = TODO
+
+love.filesystem = {}
+
+love.filesystem.getInfo = TODO
+
+love.filesystem.read = TODO
+
+love.filesystem.remove = TODO
+
+love.event = {}
+
+love.event.quit = TODO
+
+-- STI uses these
+love.data = {}
+love.math = {}
+love.physics = {}
