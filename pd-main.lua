4836a13e1e76465e45b242da900834d9ab20af89
David Sharp <hello@davidsharp.codes>
2023-07-05 15:00:19 +0100

    Adds an early wip main.lua for playdate

---
+++ pd-main.lua
@@ -0,0 +1,61 @@
+-- Name this file `main.lua`. Your game can use multiple source files if you wish
+-- (use the `import "myFilename"` command), but the simplest games can be written
+-- with just `main.lua`.
+
+-- You'll want to import these in just about every project you'll work on.
+
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+import "love-shim"
+import "love-main"
+
+-- Declaring this "gfx" shorthand will make your life easier. Instead of having
+-- to preface all graphics calls with "playdate.graphics", just use "gfx."
+-- Performance will be slightly enhanced, too.
+-- NOTE: Because it's local, you'll have to do it in every .lua source file.
+
+local gfx <const> = playdate.graphics
+
+
+-- A function to set up our game environment.
+
+function myGameSetUp()
+    love.load()
+end
+
+-- Now we'll call the function above to configure our game.
+-- After this runs (it just runs once), nearly everything will be
+-- controlled by the OS calling `playdate.update()` 30 times a second.
+
+myGameSetUp()
+
+-- `playdate.update()` is the heart of every Playdate game.
+-- This function is called right before every frame is drawn onscreen.
+-- Use this function to poll input, run game logic, and move sprites.
+
+function playdate.update()
+
+    -- Poll the d-pad and move our player accordingly.
+    -- (There are multiple ways to read the d-pad; this is the simplest.)
+    -- Note that it is possible for more than one of these directions
+    -- to be pressed at once, if the user is pressing diagonally.
+
+    -- david note: pass keypresses through, mapped to playdate controls
+    --   doing it here, because doesn't make sense as shim
+    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
+         love.keypressed('left')
+    end
+
+    -- Call the functions below in playdate.update() to draw sprites and keep
+    -- timers updated. (We aren't using timers in this example, but in most
+    -- average-complexity games, you will.)
+
+    gfx.sprite.update()
+    playdate.timer.updateTimers()
+
+    love.update()
+    love.draw()
+end
